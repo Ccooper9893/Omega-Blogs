9f410c7bbe90742b45a3cef0708cc0fef6e54f69
Cody Cooper <cody_codster@live.com>
2023-01-21 22:40:56 -0600

    Completed api request routes for signin and account creation

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -9,12 +9,17 @@ router.post('/signup', async (req, res) => {
             email: req.body.email,
             password: req.body.password
         });
-    
+
+        if(!newUserData) {
+            res.status(400).json({message: 'Please provide the required information!'});
+            return;
+        };
+
         req.session.save(() => {
             req.session.loggedIn = true;
+            req.session.user = newUserData;
+            res.status(201).json(newUserData);
         });
-    
-        res.status(201).json(newUserData);
 
     } catch (error) {
         res.status(500).json(error);
@@ -35,6 +40,11 @@ router.post('/signin', async (res, req) => {
             return;
         };
 
+        const validatePassword = await User.checkPassword(req.body.password);
+
+        if(!validatePassword) {
+            res.status(400).json({ message: 'Invalid username/password. Please try again!' })
+        }
 
     } catch (error) {
         res.status(500).json(error);
