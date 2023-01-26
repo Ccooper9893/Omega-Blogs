c53e0b9069b934ba771550c68d22b42581ac426b
Cody Cooper <cody_codster@live.com>
2023-01-26 03:19:13 -0600

    Commenting out test code

--- config/connection.js
+++ config/connection.js
@@ -10,14 +10,14 @@ const sequelize = process.env.JAWSDB_URL
         port: 3306,
     });
 
-const test = async () => {
-try {
-  await sequelize.authenticate();
-  console.log('Connection has been established successfully.');
-    } catch (error) {
-  console.error('Unable to connect to the database:', error);
-    }
-}
+// const test = async () => {
+// try {
+//   await sequelize.authenticate();
+//   console.log('Connection has been established successfully.');
+//     } catch (error) {
+//   console.error('Unable to connect to the database:', error);
+//     }
+// }
 
-test();
+// test();
 module.exports = sequelize; //Exports to express server.js
