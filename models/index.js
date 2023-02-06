4a97a80a44e7e9d9a47d46e67205d2e3e8b412ca
Cody Cooper <cody_codster@live.com>
2023-02-06 04:07:43 -0600

    Updated model paths

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
-const User = require('../models/User');
-const Blog = require('../models/Blog');
-const Comment = require('../models/Comment');
+const User = require('../models/User.js');
+const Blog = require('../models/Blog.js');
+const Comment = require('../models/Comment.js');
 
 //Associations
 User.hasMany(Blog, {
