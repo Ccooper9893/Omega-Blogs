0f939fe839461d3886f1ee5957dc17077072bc58
Cody Cooper <cody_codster@live.com>
2023-02-06 04:04:09 -0600

    Fixed paths for models

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
-const User = require('./User');
-const Blog = require('./Blog');
-const Comment = require('./Comment');
+const User = require('../models/User');
+const Blog = require('../models/Blog');
+const Comment = require('../models/Comment');
 
 //Associations
 User.hasMany(Blog, {
--- utils/auth.js
+++ utils/auth.js
@@ -1,11 +1,3 @@
-// const isAuth = (req, res, next) => {
-//     if (!req.session.loggedIn) {
-//         res.redirect('/login')
-//     } else {
-//         next()
-//     }
-// }
-
 const isAuth = (req, res, next) => {
     if(!req.session.loggedIn) {
         res.redirect('/login');
