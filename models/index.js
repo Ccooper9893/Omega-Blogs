68538d31635c799e6e50582f582e4e1063258df2
Cody Cooper <cody_codster@live.com>
2023-02-06 22:49:45 -0600

    Debugging

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
-const User = require('/User');
-const Blog = require('/Blog');
-const Comment = require('/Comment');
+const User = require('./User');
+const Blog = require('./Blog');
+const Comment = require('./Comment');
 
 //Associations
 User.hasMany(Blog, {
