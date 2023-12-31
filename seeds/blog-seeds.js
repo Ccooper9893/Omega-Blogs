10249e91e612bb4de9b7653be6714186f2fef219
Cody Cooper <cody_codster@live.com>
2023-12-30 21:19:18 -0600

    Updated seeds

--- seeds/blog-seeds.js
+++ seeds/blog-seeds.js
@@ -2,20 +2,55 @@ const { Blog } = require('../models');
 
 const blogData = [
     {
-        title: 'Gaming is good for your health',
-        blog_body: 'Gaming can potentially provide many skills that can help in everyday life. This includes pattern recognition, hand-eye coordination, and critical thinking.',
+        title: 'The Power of Positive Thinking',
+        blog_body: 'Positive thinking can have a profound impact on your mental and physical well-being. In this post, we explore the science behind positivity and how cultivating a positive mindset can lead to a happier and healthier life.',
         user_id: 1
-    },
-    {
-        title: 'Games you should not buy for your kids.',
-        blog_body: 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?',
+      },
+      {
+        title: 'Discovering Hidden Gems in Bookstores',
+        blog_body: 'Bookstores are treasure troves of knowledge and imagination. Join me on a journey to uncover lesser-known literary gems that may have escaped mainstream attention. From classic novels to contemporary poetry, there\'s always something new to explore!',
         user_id: 2
-    },
-    {
-        title: 'Games you should buy today!',
-        blog_body: 'Subnautica, Don\'t starve together, Surviving Mars',
+      },
+      {
+        title: 'The Science of Sleep: Unlocking Its Mysteries',
+        blog_body: 'Sleep is essential for our well-being, yet its complexities remain a mystery. In this post, we delve into the science of sleep, exploring the stages of the sleep cycle, the importance of quality rest, and tips for achieving a good night\'s sleep.',
+        user_id: 3
+      },
+      {
+        title: 'Embracing Minimalism for a Fulfilling Life',
+        blog_body: 'In a world filled with constant distractions, minimalism offers a path to a more intentional and fulfilling life. Learn how simplifying your surroundings and decluttering your mind can lead to increased clarity, focus, and overall happiness.',
         user_id: 1
-    }
+      },
+      {
+        title: 'The Impact of Technology on Human Connection',
+        blog_body: 'While technology has brought us closer in many ways, it has also changed the nature of human connection. Explore the positive and negative effects of technology on relationships, communication, and the sense of community in the digital age.',
+        user_id: 2
+      },
+      {
+        title: 'Exploring National Parks: A Nature Lover\'s Guide',
+        blog_body: 'National parks are a haven for nature enthusiasts, offering breathtaking landscapes and diverse ecosystems. Join me as we embark on a virtual journey through some of the most awe-inspiring national parks, celebrating the beauty of the great outdoors.',
+        user_id: 3
+      },
+      {
+        title: 'The Art of Time Management in a Busy World',
+        blog_body: 'In a fast-paced world, effective time management is crucial for personal and professional success. Discover practical tips, techniques, and tools to help you prioritize tasks, stay organized, and make the most of your valuable time.',
+        user_id: 1
+      },
+      {
+        title: 'Culinary Adventures: Exploring Global Cuisines',
+        blog_body: 'Dive into the world of international flavors and culinary traditions. From street food in Asia to gourmet delights in Europe, join me on a gastronomic journey as we explore the richness and diversity of global cuisines.',
+        user_id: 2
+      },
+      {
+        title: 'The Impact of Exercise on Mental Health',
+        blog_body: 'Physical activity is not only beneficial for the body but also plays a crucial role in maintaining mental health. Explore the connection between exercise and mental well-being, and discover how incorporating movement into your routine can boost mood and reduce stress.',
+        user_id: 3
+      },
+      {
+        title: 'Unveiling the Beauty of Artisanal Craftsmanship',
+        blog_body: 'In a world dominated by mass production, artisanal craftsmanship stands out as a testament to skill, dedication, and creativity. Join me in celebrating the beauty of handmade goods and the artisans who bring them to life.',
+        user_id: 1
+      }
 ];
 
 const seedBlogs = () => Blog.bulkCreate(blogData);
--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -2,30 +2,55 @@ const { Comment } = require('../models');
 
 const commentData = [
     {
-        text: 'I like this blog ton! Thank you.',
-        user_id: 1,
-        blog_id: 3,
-    },
-    {
-        text: 'Terrible job, you suck!',
+        text: 'This article inspired me to adopt a more positive mindset. Thank you for the insightful content!',
+        user_id: 2,
+        blog_id: 1,
+      },
+      {
+        text: 'I never realized how much I enjoy exploring bookstores until I read your post. Can\'t wait to discover hidden gems!',
         user_id: 3,
         blog_id: 2,
-    },
-    {
-        text: 'I disagree with your opinions.',
+      },
+      {
+        text: 'The science of sleep is fascinating! Your post provided valuable information and practical tips. I\'m looking forward to better sleep!',
         user_id: 1,
         blog_id: 3,
-    },
-    {
-        text: 'Great job, amazing article!',
+      },
+      {
+        text: 'I\'ve started decluttering my space after reading about minimalism here. It truly makes a difference in my daily life. Thanks!',
+        user_id: 2,
+        blog_id: 4,
+      },
+      {
+        text: 'Your exploration of the impact of technology resonates with my experiences. It\'s a thought-provoking read!',
+        user_id: 3,
+        blog_id: 5,
+      },
+      {
+        text: 'I\'m planning a trip to a national park now! Your vivid descriptions ignited my wanderlust. Great post!',
         user_id: 1,
-        blog_id: 3,
-    },
-    {
-        text: 'Cool!',
+        blog_id: 6,
+      },
+      {
+        text: 'Time management has been a struggle for me, and your post provided practical strategies. Thank you!',
+        user_id: 2,
+        blog_id: 7,
+      },
+      {
+        text: 'Your culinary adventures series is making me hungry! Can\'t wait to try some global cuisines. Keep it up!',
+        user_id: 3,
+        blog_id: 8,
+      },
+      {
+        text: 'I started incorporating exercise into my routine, and I already feel the positive effects on my mental health. Your post was motivating!',
         user_id: 1,
-        blog_id: 2,
-    }
+        blog_id: 9,
+      },
+      {
+        text: 'Artisanal craftsmanship is truly a form of art. Your appreciation for handmade goods is evident in your writing. Well done!',
+        user_id: 2,
+        blog_id: 10,
+      },
 ];
 
 const seedComments = () => Comment.bulkCreate(commentData);
